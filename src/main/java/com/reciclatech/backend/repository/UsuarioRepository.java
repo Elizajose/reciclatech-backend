eb71a45fc884699da00fae4478de65a6b9437b1c
Elizandro <elizandro.nascimento@di2win.com>
2026-01-18 15:48:43 -0300

    Repository

--- src/main/java/com/reciclatech/backend/repository/UsuarioRepository.java
+++ src/main/java/com/reciclatech/backend/repository/UsuarioRepository.java
@@ -3,10 +3,14 @@ package com.reciclatech.backend.repository;
 import com.reciclatech.backend.model.Usuario; // <--- Importante: Importar sua Entidade
 import org.springframework.data.jpa.repository.JpaRepository; // <--- Importante
 import java.util.Optional; // <--- Importante para evitar erros se não achar ninguém
+import com.reciclatech.backend.model.Usuario;
+import com.reciclatech.backend.model.StatusColeta; // <--- IMPORTANTE 1
+import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // O Spring lê "findByCpf" e cria automaticamente o SQL:
     // SELECT * FROM usuario WHERE cpf = ?
     Optional<Usuario> findByTelefone(String telefone);
+    List<Usuario> findByStatus(StatusColeta status); // <--- IMPORTANTE 2
 }
